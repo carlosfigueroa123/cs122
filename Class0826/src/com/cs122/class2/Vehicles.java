4383d92a2ef7f4777b8da7f8712fa96817569a63
dhruv857 <dgandhi@pace.edu>
2020-08-26 14:20:30 -0400

    Initialization by professor

---
+++ Class0826/src/com/cs122/class2/Vehicles.java
@@ -0,0 +1,28 @@
+package com.cs122.class2;
+
+public class Vehicles {
+	//	
+	public String typeOfCar;
+	public final int GEARS = 8;
+	public int yearMake;
+	public Vehicles(String typeOfCar,int yearMake) {
+		this.typeOfCar = typeOfCar;
+		this.yearMake = yearMake;
+	}
+	//camelCasing
+	// getDetails <--- this format
+	
+	//snake casing
+	// get_details
+//	public <return_type> getDetails() {
+//		
+//	}
+	
+	public static void getDetails(String randomThing) {
+		System.out.println("Vehicle "+ randomThing );
+	}
+	
+	public void printDetails() {
+		System.out.println(this.typeOfCar);
+	}
+}
