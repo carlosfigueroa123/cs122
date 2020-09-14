3f62a26b67fcc88bf820a56ccf365ac727f6ee95
dhruv857 <dgandhi@pace.edu>
2020-09-14 09:06:11 -0400

    Assignment 1 feedback

--- Assignments/src/com/cs122/assignments/Activities.java
+++ Assignments/src/com/cs122/assignments/Activities.java
@@ -1,35 +1,37 @@
 package com.cs122.assignments;
 
 public class Activities {
-	   public static String activityName = "soccer";
-			 public static int suggestedHours = 2;
-			 public static String locationType = "corn field";
-	     public static boolean oddHours = false;
-		    
-public Activities(){
-	suggestedHours = 2;
-	locationType = "corn field";
-	activityName = "soccer";
+	// format your code to have proper indent, that way it is much more readable
+
+	public static String activityName = "soccer";
+	public static int suggestedHours = 2;
+	public static String locationType = "corn field";
+	// you dont need this variable
+	public static boolean oddHours = false;
+
+	public Activities() {
+		suggestedHours = 2;
+		locationType = "corn field";
+		activityName = "soccer";
+	}
+
+	public void printActivity() {
+		System.out.println(
+				"The " + locationType + " activity " + activityName + " is suggested for " + suggestedHours + " hours");
+
+	}
+
+	public void preferredActivity(String activityPreferred) {
+		System.out.println("I prefer " + activityPreferred + " over " + activityName);
+
+	}
+
+	public boolean getOddHours() {
+		if (suggestedHours % 2 == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 }
-	 public void printActivity() {
-	    	System.out.println("The " + locationType + " activity " + activityName + " is suggested for " + suggestedHours + " hours");
-	    	
-	    }
-
-	  public void preferredActivity(String activityPreferred){
-	    System.out.println("I prefer " + activityPreferred + " over " + activityName);
-
-	  }
-	  
-	  public boolean getOddHours(){
-	if (suggestedHours%2 == 0){
-	  return false;
-	}
-	else{
-	  return true;
-	}
-	  }
-	  
-		
-	}
-
--- Assignments/src/com/cs122/assignments/ActivitiesHelper.java
+++ Assignments/src/com/cs122/assignments/ActivitiesHelper.java
@@ -2,23 +2,21 @@ package com.cs122.assignments;
 
 public class ActivitiesHelper {
 
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+
 		boolean odd;
-		
-		  Activities act = new Activities();
 
-		  act.printActivity();
-		  act.preferredActivity("tennis");
-		  odd = act.getOddHours();
+		Activities act = new Activities();
 
-		  if (odd == true){
-		    System.out.println("Odd");
-		  }
-		  else{
-		    System.out.println("FALSE");
-		  }
+		act.printActivity();
+		act.preferredActivity("tennis");
+		odd = act.getOddHours();
 
-		
-}
+		if (odd == true) {
+			System.out.println("Odd");
+		} else {
+			System.out.println("FALSE");
+		}
+
+	}
 }
