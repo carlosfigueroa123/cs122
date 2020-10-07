b1da69019a42e4860fcdb8c679526cbc4a921d5b
carlosfigueroa123 <70162843+carlosfigueroa123@users.noreply.github.com>
2020-10-07 15:53:32 -0400

    java exceptions lab october 5th, lift and building.

---
+++ ClassLabs/src/com/cs122/classlabs/chap11/Building.java
@@ -0,0 +1,12 @@
+package com.cs122.classlabs.chap11;
+
+public class Building {
+
+static public void main(String[] args){
+	
+	Lift broadway = new Lift();
+	
+	broadway.floor1();
+	
+}
+}
---
+++ ClassLabs/src/com/cs122/classlabs/chap11/Lift.java
@@ -0,0 +1,19 @@
+package com.cs122.classlabs.chap11;
+
+public class Lift {
+
+	
+	public void floor1() {
+		System.out.println("First floor");
+		floor2();
+	}
+	
+	public void floor2() {
+		System.out.println("Second floor");
+		int numerator = 0;
+		int denominator = 0;
+		int result;
+		
+		result = numerator / denominator;
+	}
+}
