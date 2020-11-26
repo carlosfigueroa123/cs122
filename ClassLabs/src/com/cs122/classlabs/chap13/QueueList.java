973fad43d0c26a6a07a62e65f679bf4fcd2ad38a
carlosfigueroa123 <70162843+carlosfigueroa123@users.noreply.github.com>
2020-11-25 23:18:11 -0500

    Stacklist and queuelist assignments completed.

--- ClassLabs/src/com/cs122/classlabs/chap13/QueueList.java
+++ ClassLabs/src/com/cs122/classlabs/chap13/QueueList.java
@@ -1,15 +1,75 @@
 package com.cs122.classlabs.chap13;
 
+
 public class QueueList {
+	private Node list;
 	
-	private class Node{
-		public String data;
+	public QueueList() {
+		list = null;
+	}
+//	
+//	public void enqueue(String x)//this method adds them at the beginning, but couldnt solve to dequeue at the end
+//    {
+//	    Node oldtop = list;
+//	    list = new Node(x);
+//	    list.data = x;
+//	    list.link = oldtop;
+//    }
+	public void enqueue(String x)
+    {
+        Node newNode = new Node(x);
+        Node current;//pointer for the beginning to search for the end of the list
+
+        if (list == null)
+            list = newNode;
+        else
+        {
+            current = list;
+            while (current.link != null)
+                current = current.link;//going forward
+            current.link = newNode;//assigning, this is where we add node, the new magazine introduced
+        }
+    }
+	
+	public String dequeue() {//this will dequeue the first one, not the last one. Do I have to iterate through it?
+		String data = list.data;
+	    list = list.link;
+	    return data;
+	}
+	
+	public void print() {
+		
+		 Node current = list;
+		 
+
+	        while (current != null)
+	        {
+	           System.out.print(current.data + "<-----");//we use data since it comes from intnode class (object)
+	           current = current.link;// as in the last while loop, for it to keep running
+	        }
+	        System.out.println(" ");
+	        
+	      
+	}
+	
+	public boolean isEmpty(){ 
+	    if (list == null) {
+	    	return true;
+	    }
+	    else {
+	    	return false;
+	    }
+	  }
+	
+	private class Node {
+		public String data;//this is to store the data of the node
 		public Node link;
+		
+		public Node(String x1)
+	    {
+	        data = x1;
+	        link = null;
+	    }
+		
 	}
-	public Queues() {
-	}
-	
-	public void enqueue(String item) {}
-	public String dequeue() {}
-	public boolean empty() {}
 }
--- ClassLabs/src/com/cs122/classlabs/chap13/QueueListDriver.java
+++ ClassLabs/src/com/cs122/classlabs/chap13/QueueListDriver.java
@@ -3,7 +3,17 @@ package com.cs122.classlabs.chap13;
 public class QueueListDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		QueueList queue = new QueueList();
+		
+		queue.enqueue("Carlos");
+		queue.enqueue("David");
+		queue.print();
+		queue.enqueue("Mack");
+		queue.print();
+		System.out.println(queue.dequeue());
+		queue.print();
+		System.out.println(queue.isEmpty());
 
 	}
 
